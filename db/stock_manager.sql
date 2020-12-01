227b9ed128b2ed33962d37c30c884be2f1d9600a
Ailsa Young <ailsa.young16@hotmail.co.uk>
2020-12-01 12:01:43 +0000

    added genre table to database and genre_id to albums table

--- db/stock_manager.sql
+++ db/stock_manager.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS albums;
 DROP TABLE IF EXISTS artists;
 DROP TABLE IF EXISTS labels;
+DROP TABLE IF EXISTS genres;
 
 CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
@@ -12,11 +13,16 @@ CREATE TABLE labels (
     name VARCHAR(255)
 );
 
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255)
+);
+
 CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     artist_id SERIAL REFERENCES artists(id) ON DELETE CASCADE,
-    genre VARCHAR(255),
+    genre_id SERIAL REFERENCES genres(id) ON DELETE CASCADE,
     price FLOAT,
     cost_price FLOAT,
     release_year INT,
